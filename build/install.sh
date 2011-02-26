7ae9eceb784c4de1ec734f4ad79774ad11ac8907
kkuramitsu@gmail.com <kkuramitsu@gmail.com@11e8e06f-943d-0410-b0b4-4be84c385469>
2011-02-26 14:41:02 +0000

    modified install.h
    git-svn-id: http://konoha.googlecode.com/svn/trunk/konoha@6 11e8e06f-943d-0410-b0b4-4be84c385469

--- build/install.sh
+++ build/install.sh
@@ -10,7 +10,7 @@ if test $uname = "Darwin" ; then
 	libext="dylib"
 fi
 
-echo "install ${ktarget} from ${kpath}"
+echo "install ${ktarget} to ${kpath}"
 
 if [ ! \( -d ${kpath} \) ] ; then
 	echo "setup ${kpath}"
@@ -20,12 +20,16 @@ if [ ! \( -d ${kpath} \) ] ; then
 	mkdir ${kpath}/include
 fi
 
+if [ ! \( -d ${kpath}/include/${ktarget} \) ] ; then
+	mkdir ${kpath}/include/${ktarget}
+fi
+
 echo "copying.."
 
 cp ${build}/${ktarget} ${kpath}/bin/${ktarget}
 cp ${build}/lib${ktarget}.${libext} ${kpath}/lib/lib${ktarget}.${libext}
 cp include/${ktarget}.h ${kpath}/include/${ktarget}.h
-cp -r include/${ktarget} ${kpath}/include
+cp include/${ktarget}/*.h ${kpath}/include/${ktarget}
 
 if [ -h ${kpath}/bin/konoha ] ; then
 	rm -f ${kpath}/bin/konoha
