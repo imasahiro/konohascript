b1f9ee983830f331becadaeb3941dac7d419c9fe
kkuramitsu@gmail.com <kkuramitsu@gmail.com@11e8e06f-943d-0410-b0b4-4be84c385469>
2011-07-25 12:56:53 +0000

    updated ac
    git-svn-id: http://konoha.googlecode.com/svn/trunk/konoha@454 11e8e06f-943d-0410-b0b4-4be84c385469

--- ac/ac2.sh
+++ ac/ac2.sh
@@ -4,6 +4,6 @@ logfile="`uname -s`-`uname -r`-`konoha -V0`.txt"
 
 find ./ac -maxdepth 2 -name '*.k' | while read utest; do
 	echo "Testing $utest .."
-	konoha -l "+$logfile" $utest
+	konoha -l "+$logfile" $utest || echo "[SEGV] $utest" >> $logfile
 done
 
