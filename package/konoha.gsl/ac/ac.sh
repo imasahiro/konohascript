863e9bb521b379cacfc34338340009dd820145cd
wakamori111@gmail.com <wakamori111@gmail.com@11e8e06f-943d-0410-b0b4-4be84c385469>
2011-10-14 14:02:52 +0000

    add GslPoly APIs, samples and ac written by Y. Wakamatsu
    git-svn-id: http://konoha.googlecode.com/svn/trunk/konoha@680 11e8e06f-943d-0410-b0b4-4be84c385469

---
+++ package/konoha.gsl/ac/ac.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PKGDIR="."
+
+echo ... start $PKGDIR/ac/${0##*/} ...
+
+logfile=ac.log
+
+if [ -e $PKGDIR/$logfile ] ;then
+	rm $PKGDIR/$logfile
+fi
+
+for utest in `find $PKGDIR -name "*.k"`; do
+    echo $utest
+    konoha -l "+$PKGDIR/$logfile" $utest
+done
+
+
+echo ... all tests were done ...
--- package/konoha.gsl/src/poly.c
+++ package/konoha.gsl/src/poly.c
@@ -28,6 +28,7 @@
 // LIST OF CONTRIBUTERS
 //  kimio - Kimio Kuramitsu, Yokohama National University, Japan
 //  shinpei - Shinpei Nakata, Yokohama National University, Japan
+//  yukkiwakka - Yuuki Wakamatsu, Yokohama National University, Japan
 // **************************************************************************
 
 #define USE_STRUCT_Tuple
@@ -45,6 +46,58 @@ DEFAPI(void) defGslPoly(CTX ctx, knh_class_t cid, knh_ClassDef_t *cdef)
 	cdef->name = "GslPoly";
 }
 
+//## @Native float GslPoly.eval(float[] c, float x);
+KMETHOD GslPoly_eval(CTX ctx, knh_sfp_t *sfp _RIX)
+{
+	knh_Array_t *c = sfp[1].a;
+	double x = sfp[2].fvalue;
+	double r = gsl_poly_eval(c->flist, knh_Array_size(c), x);
+	RETURNf_(r);
+}
+
+//## @Native int GslPoly.evalDerivs(float[] c, float x, float[] res);
+KMETHOD GslPoly_evalDerivs(CTX ctx, knh_sfp_t *sfp _RIX)
+{
+	knh_Array_t *c = sfp[1].a;
+	double x = sfp[2].fvalue;
+	knh_Array_t *res = sfp[3].a;
+	int n = gsl_poly_eval_derivs(c->flist, knh_Array_size(c), x, res->flist, knh_Array_size(res));
+	RETURNi_(n);
+}
+
+//## @Native int GslPoly.ddInit(float[] dd, float[] xa, double ya[]);
+KMETHOD GslPoly_ddInit(CTX ctx, knh_sfp_t *sfp _RIX)
+{
+	knh_Array_t *dd = sfp[1].a;
+	knh_Array_t *xa = sfp[2].a;
+	knh_Array_t *ya = sfp[3].a;
+	int size = knh_Array_size(dd);
+	int r = gsl_poly_dd_init(dd->flist, xa->flist, ya->flist, size);
+	RETURNi_(r);
+}
+
+//## @Native float GslPoly.ddEval(float[] dd, float[] xa, double x);
+KMETHOD GslPoly_ddEval(CTX ctx, knh_sfp_t *sfp _RIX)
+{
+	knh_Array_t *dd = sfp[1].a;
+	knh_Array_t *xa = sfp[2].a;
+	double x = sfp[3].fvalue;
+	double r = gsl_poly_dd_eval(dd->flist, xa->flist, knh_Array_size(xa), x);
+	RETURNf_(r);
+}
+
+//## @Native int GslPoly.ddTaylor(float[] c, float xp, float[] dd, float[] xa, double w[]);
+KMETHOD GslPoly_ddTaylor(CTX ctx, knh_sfp_t *sfp _RIX)
+{
+	knh_Array_t *c = sfp[1].a;
+	double xp = sfp[2].fvalue;
+	knh_Array_t *dd = sfp[3].a;
+	knh_Array_t *xa = sfp[4].a;
+	knh_Array_t *w = sfp[5].a;
+	int r = gsl_poly_dd_taylor(c->flist, xp, dd->flist, xa->flist, knh_Array_size(dd), w->flist);
+	RETURNi_(r);
+}
+
 //## @Native Tuple<float,float> GslPoly.solveQuadratic(float a, float b, float c);
 KMETHOD GslPoly_solveQuadratic(CTX ctx, knh_sfp_t *sfp _RIX)
 {
@@ -57,6 +110,72 @@ KMETHOD GslPoly_solveQuadratic(CTX ctx, knh_sfp_t *sfp _RIX)
 	t->ffields[0] = (knh_float_t)x0;
 	t->ffields[1] = (knh_float_t)x1;
 	RETURN_(t);
+}
+
+//## @Native Tuple<float,float,float> GslPoly.solveCubic(float a, float b, float c);
+KMETHOD GslPoly_solveCubic(CTX ctx, knh_sfp_t *sfp _RIX)
+{
+	double a = Float_to(double, sfp[1]);
+	double b = Float_to(double, sfp[2]);
+	double c = Float_to(double, sfp[3]);
+	double x0 = 0.0, x1 = 0.0, x2 = 0.0;
+	gsl_poly_solve_cubic(a, b, c, &x0, &x1, &x2);
+	knh_Tuple_t *t = (knh_Tuple_t*)new_ReturnObject(ctx, sfp);
+	t->ffields[0] = (knh_float_t)x0;
+	t->ffields[1] = (knh_float_t)x1;
+	t->ffields[2] = (knh_float_t)x2;
+	RETURN_(t);
+}
+
+//## @Native float[] GslPoly.complexSolve(float[] a, GslPolyComplexWorkspace ws);
+KMETHOD GslPoly_complexSolve(CTX ctx, knh_sfp_t *sfp _RIX)
+{
+	knh_Array_t *a = sfp[1].a;
+	int size = knh_Array_size(a);
+	knh_Array_t *z = new_Array(ctx, CLASS_Float, (size - 1) * 2);
+	z->size = (size - 1) * 2;
+	gsl_poly_complex_workspace *ws;
+	if(IS_NULL(sfp[2].o)) {
+		ws = gsl_poly_complex_workspace_alloc(size);
+	}else{
+		ws = RawPtr_to(gsl_poly_complex_workspace *, sfp[2]);
+	}
+	gsl_poly_complex_solve(a->flist, size, ws, z->flist);
+	if(IS_NULL(sfp[2].o)) {
+		gsl_poly_complex_workspace_free(ws);
+	}
+	RETURN_(z);
+}
+
+/* ------------------------------------------------------------------------ */
+
+static void GslPolyComplexWorkspace_init(CTX ctx, knh_RawPtr_t *po)
+{
+	po->rawptr = NULL;
+}
+
+static void GslPolyComplexWorkspace_free(CTX ctx, knh_RawPtr_t *po)
+{
+	if (po->rawptr != NULL) {
+		gsl_poly_complex_workspace_free((gsl_poly_complex_workspace *)po->rawptr);
+		po->rawptr = NULL;
+	}
+}
+
+DEFAPI(void) defGslPolyComplexWorkspace(CTX ctx, knh_class_t cid, knh_ClassDef_t *cdef)
+{
+	cdef->name = "GslPolyComplexWorkspace";
+	cdef->init = GslPolyComplexWorkspace_init;
+	cdef->free = GslPolyComplexWorkspace_free;
+}
+
+//## @Native GslPolyComplexWorkspace GslPolyComplexWorkspace.new(int size);
+KMETHOD GslPolyComplexWorkspace_new(CTX ctx, knh_sfp_t *sfp _RIX)
+{
+	size_t size = Int_to(size_t, sfp[1]);
+	knh_RawPtr_t *p = sfp[0].p;
+	p->rawptr = gsl_poly_complex_workspace_alloc(size);
+	RETURN_(p);
 }
 
 #ifdef __cplusplus
