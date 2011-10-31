b4413af13c91deb26a226b02fd3906265dc9d290
shinpei.nakata <shinpei.nakata@11e8e06f-943d-0410-b0b4-4be84c385469>
2011-10-31 04:27:15 +0000

    konoha.sql: added ntraces for mysql.c
    git-svn-id: http://konoha.googlecode.com/svn/trunk/konoha@723 11e8e06f-943d-0410-b0b4-4be84c385469

--- package/konoha.sql/src/mysql.c
+++ package/konoha.sql/src/mysql.c
@@ -29,6 +29,7 @@
 //  kimio - Kimio Kuramitsu, Yokohama National University, Japan
 //  uh    - Yutaro Hiraoka, Yokohama National University, Japan
 //  yoan  - Motoki Yoan, Yokohama National University, Japan
+//  shinpei_nkt  - Shinpei Nakata, Yokohama National University, Japan (ntrace)
 // **************************************************************************
 
 #ifdef __cplusplus 
@@ -36,7 +37,7 @@ extern "C" {
 #endif
 
 #include "../include/konoha_query.h"
-#ifdef K_USING_MYSQL
+//#ifdef K_USING_MYSQL
 #include <mysql.h>
 
 #define MYSQL_USER_MAXLEN 16
@@ -46,11 +47,12 @@ extern "C" {
 
 /* ======================================================================== */
 
+/*
 static void knh_mysql_perror(CTX ctx, MYSQL *db, int r)
 {
 //	KNH_SYSLOG(ctx, LOG_WARNING, "MysqlError", "'%s'", mysql_error(db));
 }
-
+*/
 static inline knh_bytes_t knh_bytes_last(knh_bytes_t t, knh_intptr_t loc)
 {
 	knh_bytes_t t2 = {{t.text + loc}, t.len - loc};
@@ -79,11 +81,18 @@ knh_qconn_t *MYSQL_qopen(CTX ctx, knh_bytes_t url)
 	pdbnm = (dbnm[0]) ? dbnm : NULL;
 	
 	MYSQL *db = mysql_init(NULL);
-	if (!mysql_real_connect(db, phost, puser, ppass, pdbnm, port, NULL, 0)) {
-		//knh_mysql_perror(ctx, db, 0);
+	knh_ldata_t ldata[] = {LOG_END};
+	KNH_NTRACE(ctx, "mysql_init", (db != NULL) ? K_OK : K_FAILED, ldata);
+	knh_ldata_t ldata2[] = {LOG_s("host", phost), LOG_s("user", puser), /* LOG_s("passwd", ppass), */
+							LOG_s("dbname", pdbnm), LOG_i("port", port), LOG_i("errno", mysql_errno(db)), 
+							LOG_s("error", mysql_error(db)), LOG_END};
+	db = mysql_real_connect(db, phost, puser, ppass, pdbnm, port, NULL, 0);
+	KNH_NTRACE(ctx, "mysql_real_connect", (db != NULL) ? K_OK : K_FAILED, ldata2);
+	/*	if (!mysql_real_connect(db, phost, puser, ppass, pdbnm, port, NULL, 0)) {
+		knh_mysql_perror(ctx, db, 0);
 		mysql_close(db);
 		db = NULL;
-	}
+		}*/
 	return (knh_qconn_t*)db;
 }
 /* ------------------------------------------------------------------------ */
@@ -101,8 +110,10 @@ static inline knh_bytes_t new_bytes(char *c_buf)
 int MYSQL_qnext(CTX ctx, knh_qcur_t *qcur, struct knh_ResultSet_t *rs)
 {
 	MYSQL_ROW row;
+	knh_ldata_t ldata[] = {LOG_END};
 	if ((row = mysql_fetch_row((MYSQL_RES*)qcur)) != NULL) {
-		int i;
+		KNH_NTRACE(ctx, "mysql_fetch_row", K_OK, ldata);
+		int i;		
 		knh_int_t ival;
 		knh_float_t fval;
 		for (i = 0; i < DP(rs)->column_size; i++) {
@@ -111,32 +122,23 @@ int MYSQL_qnext(CTX ctx, knh_qcur_t *qcur, struct knh_ResultSet_t *rs)
 				continue;
 			}
 			switch (DP(rs)->column[i].dbtype) {
-			case MYSQL_TYPE_TINY:
-			case MYSQL_TYPE_SHORT:
-			case MYSQL_TYPE_INT24:
-			case MYSQL_TYPE_LONG:
+			case MYSQL_TYPE_TINY:			case MYSQL_TYPE_SHORT:
+			case MYSQL_TYPE_INT24:			case MYSQL_TYPE_LONG:
 			//case MYSQL_TYPE_LONGLONG:
 			case MYSQL_TYPE_YEAR:
 				knh_bytes_parseint(B(row[i]), &ival);
 				ResultSet_setInt(ctx, rs, i, ival);
 				break;
-			case MYSQL_TYPE_FLOAT:
-			case MYSQL_TYPE_DOUBLE:
+			case MYSQL_TYPE_FLOAT:			case MYSQL_TYPE_DOUBLE:
 				knh_bytes_parsefloat(B(row[i]), &fval);
 				ResultSet_setFloat(ctx, rs, i, fval);
 				break;
-			case MYSQL_TYPE_NEWDECIMAL:
-			case MYSQL_TYPE_STRING:
-			case MYSQL_TYPE_VAR_STRING:
-			case MYSQL_TYPE_TINY_BLOB:
-			case MYSQL_TYPE_BLOB:
-			case MYSQL_TYPE_MEDIUM_BLOB:
-			case MYSQL_TYPE_LONG_BLOB:
-			case MYSQL_TYPE_BIT:
-			case MYSQL_TYPE_TIME:
-			case MYSQL_TYPE_DATE:
-			case MYSQL_TYPE_DATETIME:
-			case MYSQL_TYPE_TIMESTAMP:
+			case MYSQL_TYPE_NEWDECIMAL:			case MYSQL_TYPE_STRING:
+			case MYSQL_TYPE_VAR_STRING:			case MYSQL_TYPE_TINY_BLOB:
+			case MYSQL_TYPE_BLOB:			case MYSQL_TYPE_MEDIUM_BLOB:
+			case MYSQL_TYPE_LONG_BLOB:			case MYSQL_TYPE_BIT:
+			case MYSQL_TYPE_TIME:			case MYSQL_TYPE_DATE:
+			case MYSQL_TYPE_DATETIME:			case MYSQL_TYPE_TIMESTAMP:
 				ResultSet_setText(ctx, rs, i, B(row[i]));
 				break;
 			case MYSQL_TYPE_NULL:
@@ -145,8 +147,10 @@ int MYSQL_qnext(CTX ctx, knh_qcur_t *qcur, struct knh_ResultSet_t *rs)
 				ResultSet_setNULL(ctx, rs, i);
 				break;
 			}
-		}
+		} /* for */
 		return 1; /* CONTINUE */
+	} else {
+		KNH_NTRACE(ctx, "mysql_fetch_row", K_FAILED, ldata);
 	}
 	return 0; /* NOMORE */
 }
@@ -156,30 +160,40 @@ int MYSQL_qnext(CTX ctx, knh_qcur_t *qcur, struct knh_ResultSet_t *rs)
 knh_qcur_t *MYSQL_query(CTX ctx, knh_qconn_t *hdr, knh_bytes_t sql, knh_ResultSet_t *rs)
 {
 	MYSQL_RES *res = NULL;
-	if (hdr == NULL) {
+	MYSQL *db = (MYSQL*)hdr;
+	if (db == NULL) {
 		/* return NULL */
 	}
 	else if (rs == NULL) {
 		/* Connection.exec */
-		int r = mysql_query((MYSQL*)hdr, sql.text);
-		if(r > 0) {
-			knh_mysql_perror(ctx, (MYSQL*)hdr, r);
-		}
+		int r = mysql_query(db, sql.text);
+		knh_ldata_t ldata[] = {LOG_s("query", sql.text), LOG_i("errno", mysql_errno(db)), LOG_END};
+		KNH_NTRACE(ctx, "mysql_query", (r == 0) ? K_OK : K_FAILED, ldata);
+		/*if(r > 0) {
+			knh_mysql_perror(ctx, (MYSQL*)db, r);
+			}*/
 	}
 	else {
 		/* Connection.query */
-		int r = mysql_query((MYSQL*)hdr, sql.text);
-		if (r > 0) { // QUERY ERROR
-			knh_mysql_perror(ctx, (MYSQL*)hdr, r);
-		}
-		else {
-			res = mysql_store_result((MYSQL*)hdr);
+		int r = mysql_query((MYSQL*)db, sql.text);
+		knh_ldata_t ldata[] = {LOG_s("query", sql.text), LOG_i("errno", mysql_errno(db)),
+							   LOG_s("error", mysql_error(db)), LOG_END};
+		KNH_NTRACE(ctx, "mysql_query", (r == 0) ? K_OK : K_FAILED, ldata);
+		if (r == 0) { 
+			res = mysql_store_result((MYSQL*)db);
+			knh_ldata_t ldata2[] = {LOG_i("errno", mysql_errno(db)), LOG_s("error", mysql_error(db)),
+									LOG_END};
 			if (res == NULL) { // NULL RESULT
-				mysql_free_result(res);
-				knh_mysql_perror(ctx, (MYSQL*)hdr, 0);
+				if (mysql_errno(db) != 0) {
+				  mysql_free_result(res);
+				  KNH_NTRACE(ctx, "mysql_store_result", K_FAILED, ldata2);
+				} else {
+				  KNH_NTRACE(ctx, "mysql_store_result", K_OK, ldata2);
+				}
 			}
 			else {
 				knh_ResultSet_initColumn(ctx, rs, (size_t)mysql_num_fields(res));
+				KNH_NTRACE(ctx, "mysql_store_result", K_OK, ldata2);
 				int i = 0;
 				MYSQL_FIELD *field = NULL;
 				while((field = mysql_fetch_field(res))) {
@@ -220,6 +234,8 @@ const knh_QueryDSPI_t DB__mysql = {
 /* ------------------------------------------------------------------------ */
 /* [DEFAPI] */
 
+//#endif /* MYSQL ifdef */
+
 #ifdef _SETUP
 
 DEFAPI(const knh_PackageDef_t*) init(CTX ctx, const knh_LoaderAPI_t *kapi)
@@ -228,7 +244,7 @@ DEFAPI(const knh_PackageDef_t*) init(CTX ctx, const knh_LoaderAPI_t *kapi)
 }
 
 #endif
-#endif
+
 
 #ifdef __cplusplus 
 }
