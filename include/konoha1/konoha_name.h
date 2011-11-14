1b971344510bd4aae299b68e74eb2b7fcd5c7979
masa.ide.on@gmail.com <masa.ide.on@gmail.com@11e8e06f-943d-0410-b0b4-4be84c385469>
2011-11-14 12:06:57 +0000

    remove go token
    git-svn-id: http://konoha.googlecode.com/svn/trunk/konoha@803 11e8e06f-943d-0410-b0b4-4be84c385469

--- include/konoha1/konoha_name.h
+++ include/konoha1/konoha_name.h
@@ -3,7 +3,7 @@
 /* ------------------------------------------------------------------------ */
 /* MACROS */
 #ifndef K_REVISION
-#define K_REVISION                      795
+#define K_REVISION                      800
 #endif
 #define K_BUILDID                       1187
 
