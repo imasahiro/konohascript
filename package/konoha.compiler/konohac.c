98b5f90532ec588a0e4d724f32c6c8871ab83810
Masahiro Ide <imasahiro@users.sourceforge.jp>
2011-12-20 16:31:53 +0900

    refactor compiler package

--- package/konoha.compiler/konohac.c
+++ package/konoha.compiler/konohac.c
@@ -36,32 +36,110 @@ static kMethod *load_method(CTX ctx, kclass_t cid, kbytes_t t)
     return mtd;
 }
 
+static enum compile_mode {
+    EMIT_IR,
+    EMIT_LLVM,
+    EMIT_JS,
+    EMIT_NOP
+} compile_mode = EMIT_IR;
+static const char * codegenerator_file[] = {
+    NULL,
+    "konoha.compiler.llvm",
+    "konoha.compiler.js",
+};
+
+static const char *parse_option(int argc, const char *argv[])
+{
+    int i, argc_ = 0;
+    const char *argv_[argc];
+    if (argc <= 1) {
+        fprintf(stderr, "usage: %s script.k\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    for (i = 0; i < argc; ++i) {
+        kbytes_t t = B(argv[i]);
+        if (knh_bytes_equals(t, STEXT("--emit-llvm"))) {
+            compile_mode = EMIT_LLVM;
+            continue;
+        }
+        else if (knh_bytes_equals(t, STEXT("--emit-js"))) {
+            compile_mode = EMIT_JS;
+            continue;
+        }
+        argv_[argc_++] = argv[i];
+    }
+    konoha_ginit(argc_, argv_);
+    return argv_[1];
+}
+
 extern void knh_setCompileMode(CTX ctx, int mode);
+static fMethod_compile compilerAPI = NULL;
+static void CompilerAPI_enable(CTX ctx)
+{
+    fprintf(stderr, "%d:api=%p, shareApi=%p\n", __LINE__, compilerAPI, ctx->share->compilerAPI);
+    assert(compilerAPI != NULL && ctx->share->compilerAPI == NULL);
+    ctx->wshare->compilerAPI = compilerAPI;
+    compilerAPI     = NULL;
+    fprintf(stderr, "%d:api=%p, shareApi=%p\n", __LINE__, compilerAPI, ctx->share->compilerAPI);
+}
+static void CompilerAPI_disable(CTX ctx)
+{
+    assert(compilerAPI == NULL && ctx->share->compilerAPI != NULL);
+    fprintf(stderr, "%d:api=%p, shareApi=%p\n", __LINE__, compilerAPI, ctx->share->compilerAPI);
+    compilerAPI     = ctx->wshare->compilerAPI;
+    ctx->wshare->compilerAPI = NULL;
+    fprintf(stderr, "%d:api=%p, shareApi=%p\n", __LINE__, compilerAPI, ctx->share->compilerAPI);
+}
+
+#if defined(K_USING_WINDOWS_)
+#define SEPARATOR '\\'
+#else
+#define SEPARATOR '/'
+#endif
+
+static void load_codegenerator(CTX ctx)
+{
+    CompilerAPI_disable(ctx);
+    if (codegenerator_file[compile_mode]) {
+        kbytes_t t = new_bytes((char*)codegenerator_file[compile_mode]);
+        knh_loadPackage(ctx, t);
+        CWB_t cwbbuf, *cwb = CWB_open(ctx, &cwbbuf);
+        kString *s = (kString *) knh_DictMap_getNULL(ctx, ctx->share->props, STEXT("konoha.package.path"));
+        CWB_clear(cwb, 0);
+        knh_buff_addpath(ctx, cwb->ba, cwb->pos, 0, S_tobytes(s));
+        knh_buff_addpath(ctx, cwb->ba, cwb->pos, 1, STEXT("konoha.compiler"));
+        knh_buff_addpath(ctx, cwb->ba, cwb->pos, 1, STEXT("compiler"));
+        knh_buff_addpath(ctx, cwb->ba, cwb->pos, 0, STEXT(K_OSDLLEXT));
+        void *p = knh_dlopen(ctx, CWB_totext(ctx, cwb));
+        typedef void (*knh_Fpkgcomplete)(CTX);
+        knh_Fpkgcomplete pkgcomplete = (knh_Fpkgcomplete) knh_dlsym(ctx, p, "reset_compiler_api", NULL, 1);
+        if (pkgcomplete) pkgcomplete(ctx);
+        CWB_close(ctx, cwb);
+    }
+    CompilerAPI_enable(ctx);
+}
 
 int main(int argc, const char *argv[])
 {
-    if (argc <= 1) {
-        fprintf(stderr, "usage: %s script.k\n", argv[0]);
-        return 1;
-    }
-    konoha_ginit(1, argv);
+    const char *fname = parse_option(argc, argv);
     konoha_t konoha = konoha_open();
     CTX ctx = konoha;
     KONOHA_BEGIN(ctx); {
-        kString *s = new_T(argv[1]);
+        kString *s = new_T(fname);
         kPath *path = new_Path(ctx, s);
         knh_DictMap_set(ctx, ctx->share->props, new_T("script.name"), s);
         kbytes_t t = knh_bytes_nsname(S_tobytes(s));
-        knh_Script_setNSName(ctx, ctx->script,
-                new_String2(ctx, CLASS_String, t.text, t.len, SPOL_TEXT|SPOL_POOLALWAYS));
+        knh_Script_setNSName(ctx, ctx->script, new_S(t.text, t.len));
         kbytes_t pkgname = STEXT("konoha.compiler");
         knh_loadPackage(ctx, pkgname);
 
+        load_codegenerator(ctx);
         knh_setCompileMode(ctx, 1);
         knh_load(ctx, path);
 
         /* CompilerAPI->dump() */
-        kMethod *mtd = load_method(ctx, O_cid(ctx->share->konoha_compiler), STEXT("dump"));
+        kMethod *mtd = load_method(ctx,
+                O_cid(ctx->share->konoha_compiler), STEXT("dump"));
         BEGIN_LOCAL(ctx, lsfp, K_CALLDELTA+1); {
             KNH_SETv(ctx, lsfp[K_CALLDELTA].o, ctx->share->konoha_compiler);
             KNH_SCALL(ctx, lsfp, 0, mtd, 0);
