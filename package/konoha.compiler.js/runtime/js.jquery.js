697bfffed5288afbb2eea3e10a7985037f30e2a8
Shunsuke Shida <shunsuke.sida@gmail.com>
2012-01-20 13:31:43 +0900

    remove unnessesary console output

--- package/konoha.compiler.js/runtime/js.jquery.js
+++ package/konoha.compiler.js/runtime/js.jquery.js
@@ -6,12 +6,8 @@ var initJQuery = function() {
         this.rawptr = rawptr;
     }
     jquery.prototype = new konoha.Object();
-    console.log(jquery.konohaclass);
     jquery.konohaclass = "js.jquery.JQuery";
 
-    console.log(jquery.konohaclass);
     return jquery;
 }
 js.jquery.JQuery = initJQuery();
-console.log(js.jquery.JQuery.konohaclass);
-console.log(new konoha.Object().konohaclass);
