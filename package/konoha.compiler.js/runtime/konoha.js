01e749b8225c9b4f247a4883005fd1b2009f757f
Shunsuke Shida <shunsuke.sida@gmail.com>
2012-02-09 19:28:01 +0900

    rename variable named 'document'

--- package/konoha.compiler.js/runtime/konoha.js
+++ package/konoha.compiler.js/runtime/konoha.js
@@ -12,7 +12,7 @@ var konoha = function() {
     }
 }
 
-var document = function() {
+var htmldocument = function() {
 	this._body = "<html><head></head><body></body></html>";
 	this.getElementsByTagName = function(tagname) {
 		return null;
