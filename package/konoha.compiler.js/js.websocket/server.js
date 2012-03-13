cd933e0c55991212b4d9ff90b232c276b3d1144d
Shunsuke Shida <shunsuke.sida@gmail.com>
2012-03-13 21:25:15 +0900

    add othello server (this server script is written in javascript)

---
+++ package/konoha.compiler.js/js.websocket/server.js
@@ -0,0 +1,52 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+var connections = [];
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin) || connections.length == 2) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+
+    var connection = request.accept('othello-protocol', request.origin);
+	connection.sendUTF(connections.length + "");
+	connections.push(connection);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connections[0].sendUTF(message.utf8Data);
+			if (connections[1]) {
+				connections[1].sendUTF(message.utf8Data);
+			}
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
